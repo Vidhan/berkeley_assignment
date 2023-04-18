80c355fa46650286f3ffc62e67164c52dd39b32b
Vidhan Agarwal <vidhanagarwal@Vidhans-MacBook-Pro.local>
2023-04-18 14:27:47 -0700

    refactor search.py

--- search.py
+++ search.py
@@ -9,14 +9,19 @@ from bson import json_util
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-with open("secret.txt") as secret_file:
-    uri = secret_file.readline()
+
+
+def instantiate_client():
+    with open("secret.txt") as secret_file:
+        uri = secret_file.readline()
+        client = MongoClient(uri)
+        db = client["berkeley"]
+        collection = db["sediment_grain_size"]
+    return collection
 
 @app.route("/sampleid/<sample_id>")
 def show_sample_id(sample_id):
-    client = MongoClient(uri)
-    db = client["berkeley"]
-    collection = db["sediment_grain_size"]
+    collection = instantiate_client()
     sample_id = escape(sample_id)
     data = collection.find_one({"Sample_ID": sample_id}, {"_id":0,"index":0})
     if data is None:
@@ -26,9 +31,7 @@ def show_sample_id(sample_id):
 
 @app.route("/")
 def show_all_ids():
-    client = MongoClient(uri)
-    db = client["berkeley"]
-    collection = db["sediment_grain_size"]
+    collection = instantiate_client()
     data = list(collection.find({},{"_id":0,"Sample_ID":1}))
     if data is None:
         return "DB is empty"
