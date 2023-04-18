fa2d25dc6385414689a4c6c9e067c032d4153d22
Vidhan Agarwal <vidhanagarwal@Vidhans-MacBook-Pro.local>
2023-04-18 14:34:23 -0700

    improve error handling

--- search.py
+++ search.py
@@ -9,14 +9,15 @@ from bson import json_util
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-
+DB = "berkeley"
+COLLECTION = "sediment_grain_size"
 
 def instantiate_client():
     with open("secret.txt") as secret_file:
         uri = secret_file.readline()
         client = MongoClient(uri)
-        db = client["berkeley"]
-        collection = db["sediment_grain_size"]
+        db = client[DB]
+        collection = db[COLLECTION]
     return collection
 
 @app.route("/sampleid/<sample_id>")
@@ -33,7 +34,7 @@ def show_sample_id(sample_id):
 def show_all_ids():
     collection = instantiate_client()
     data = list(collection.find({},{"_id":0,"Sample_ID":1}))
-    if data is None:
+    if not data:
         return "DB is empty"
     else:
         return data
